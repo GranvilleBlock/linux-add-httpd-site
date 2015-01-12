57d3d2792d0e9145216c4b8815a2b1324d730926
Alexaksandr <nixomsk@gmail.com>
2015-01-12 18:47:59 +0600

    Update backup-mysql.sh
    Exclude performance_schema and information_schema databases from backup

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -5,4 +5,4 @@ PASS="MYSQL_USER_PASSWORD"
 FILE="BACKUP_FILE_NAME"
 DATE="DATE_FORMAT" # %Y-%m-%d-%H-%M-%S
 
-mysqldump -u $USER -p$PASS --all-databases --single-transaction | bzip2 > `date +$FILE-$DATE.sql.bz2`
+mysqldump -u $USER -p$PASS --all-databases --single-transaction --ignore-database=performance_schema --ignore-database=information_schema | bzip2 > `date +$FILE-$DATE.sql.bz2`
