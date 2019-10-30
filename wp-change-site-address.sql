7947de582e151e9fb86a773f380c5c6447989a04
mypluginbaby <57164263+mypluginbaby@users.noreply.github.com>
2019-10-30 09:22:45 +0600

    Clean SQL markup

--- wp-change-site-address.sql
+++ wp-change-site-address.sql
@@ -1,9 +1,18 @@
 SET @oldsite='http://oldsite.local';
 SET @newsite='http://newsite.ru';
-UPDATE wp_options SET option_value = REPLACE(option_value, 
-@oldsite, @newsite) WHERE option_name = 
-'home' OR option_name = 'siteurl';
-UPDATE wp_posts SET guid = REPLACE(guid, 
-@oldsite,@newsite);
-UPDATE wp_posts SET post_content = REPLACE(post_content, 
-@oldsite, @newsite);
+
+UPDATE 
+  wp_options 
+SET option_value = REPLACE(option_value, @oldsite, @newsite) 
+WHERE 
+  option_name = 'home' 
+  OR 
+  option_name = 'siteurl';
+  
+UPDATE 
+  wp_posts 
+SET guid = REPLACE(guid, @oldsite,@newsite);
+
+UPDATE 
+  wp_posts 
+SET post_content = REPLACE(post_content, @oldsite, @newsite);
